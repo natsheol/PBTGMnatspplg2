788ec6cf3556789361ee9bf278810ee177c4b109
natsheol <145530295+natsheol@users.noreply.github.com>
2024-07-31 13:54:02 +0700

    Add files via upload

---
+++ latihanpraktek1_2/src/main/java/com/mycompany/latihanpraktek1_2/Latihanpraktek1_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanpraktek1_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Latihanpraktek1_2 {
+
+    public static void main(String[] args) {
+      int a=50;
+      float b=a;
+      
+      System.out.println(a);
+      System.out.println(b);
+    }
+}
