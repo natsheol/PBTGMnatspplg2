db0d202cdc8ce25fe80cd3fdc3d8ed8676ec7665
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-02 14:11:35 +0700

    Latihan praktek 1.3

---
+++ latihanpraktek1_3/src/main/java/com/mycompany/latihanpraktek1_3/Latihanpraktek1_3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanpraktek1_3;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Latihanpraktek1_3 {
+    
+    public static String makanan;
+    public static final String minuman = "Jus Jambu";
+    public static void main(String[] args) {
+         // TODO code application logic
+         makanan ="Ayam geprek";
+         System.out.println("makanan favorit saya" + makanan);
+         System.out.println("minuman favorit saya" + minuman);
+    }
+
+}
