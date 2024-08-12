c0a957e3fdec64c29c0045140de4834c45f69329
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-12 14:02:16 +0700

    pertemuan4_1

---
+++ pertemuan4_1/src/main/java/com/mycompany/pertemuan4/Pertemuan4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan4 {
+
+    public static void main(String[] args) {
+        
+        //declare variable exercise 1
+        int num1 = 10;
+        int num2 = 3; 
+     
+        
+       
+        System.out.println("Hasil Pertambahan A+B = " + (num1 + num2));
+        System.out.println("Hasil Pengurangan A-B = " + (num1 - num2));
+        System.out.println("Hasil pengurangan AXB = " + (num1 * num2));
+        System.out.println("Hasil Pembagian A/B = " + (num1 / num2));
+       
+    }
+}
