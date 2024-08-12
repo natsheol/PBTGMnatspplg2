795f532a82772424b614188d74cdcc3683faf9eb
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-12 13:37:34 +0700

    Add files via upload

---
+++ pertemuan3_1/src/main/java/com/mycompany/pertemuan3_1/Pertemuan3_1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3_1;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan3_1 {
+
+    public static void main(String[] args) {
+      int a = 9;
+      float f = 8;
+      char ch = 'A';
+      boolean b = false;
+      
+      String string = "Hello World";
+      
+      System.out.println("int Var1" + a);
+      System.out.println("float Var2" + f);
+      System.out.println("char Var3"+ ch);
+      System.out.println("boolean Var4"+ b);
+      System.out.println("string Var5"+ string);
+    }
+}
