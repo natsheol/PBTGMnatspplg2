2003a6fc63982b7c75abfab2c071c8466797716c
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-12 13:47:23 +0700

    pertemuan3_2

---
+++ pertemuan3_2/src/main/java/com/mycompany/pertemuan3_2/Pertemuan3_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan3_2 {
+
+    public static void main(String[] args) {
+        int Nim = 2110117000;
+        String Nama = "Andika Jaya Utama";
+        String Kelas = "01SIFE020";
+        double Usia = 19.5;
+        double Height = 1.72;
+        
+        System.out.println("NIM : " + Nim);
+        System.out.println("Nama : " + Nama);
+        System.out.println("NIM : " + Nim);
+        System.out.println("Kelas : " + Kelas);
+        System.out.println("Usia : " + Usia);
+        System.out.println("Tinggi : " + Height);
+        
+    }
+}
