3f62a0200a7371b8be8fc3f3e8192e4cf8c89108
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-12 14:06:27 +0700

    pertemuan4_2

---
+++ pertemuan4_2/src/main/java/com/mycompany/pertemuan4_2/Pertemuan4_2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan4_2 {
+
+    public static void main(String[] args) {
+        //declare variable exercise 2
+        int w = 5;
+        int x = 6;
+        int y = 8 - x++;
+        int z = 8 - ++w;
+        
+        System.out.println("tampilkan nilai w,x,y,z = " + (w) + "," + (x) + "," + (y) + "," + (z));
+    }
+}
