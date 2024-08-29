d57f2bd47ca2ee32cc36d03ee8c89bcb6b5d42e5
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-29 20:45:28 +0700

    Add files via upload

---
+++ pertemuan7_1/src/main/java/com/mycompany/pertemuan7_1/Pertemuan7_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan7_1;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan7_1 {
+
+    public static void main(String[] args) {
+       int day = 2;
+       switch ( day ) {
+           case 1:
+       System.out.println("Sabtu ");
+       break;
+           case 2:
+       System.out.println("Minggu");
+       ;
+}
+    }
+}
