379c0bd898673e9ac1fd308d0bc14ac61e89b880
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-29 20:55:31 +0700

    Add files via upload

---
+++ pertemuan8_1/src/main/java/com/mycompany/pertemuan8_1/Pertemuan8_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan8_1;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan8_1 {
+
+    public static void main(String[] args) {
+        int arr[]={5,10,15,20,25,30,35,40,45,50};
+        for(int i:arr){
+        System.out.println(i);
+        }
+    }
+}
+
