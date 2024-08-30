da94333f2322951fbfe55f91b264bd62b0d92c82
natsheol <145530295+natsheol@users.noreply.github.com>
2024-08-30 09:17:20 +0700

    Add files via upload

---
+++ pertemuan9_1/src/main/java/com/mycompany/pertemuan9_1/Pertemuan9_1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan9_1;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan9_1 {
+
+    public static void main(String[] args) {
+       int i = 1;
+       while(i <= 5){
+           int j = 1;
+           while(j <= i) {
+               System.out.print("* ");
+               j++;
+           }
+           System.out.println();
+           i++;
+       }
+     }
+}
+  
