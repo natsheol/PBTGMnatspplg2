fbebaaffaabe367d1b17ef06581a055dc671a9c9
natsheol <145530295+natsheol@users.noreply.github.com>
2024-09-10 08:28:13 +0700

    Add files via upload

---
+++ pertemuan14_1/src/pertemuan14_1/kucingabstrak.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan14_1;
+
+/**
+ *
+ * @author nasozu
+ */
+public class kucingabstrak {
+    
+}
