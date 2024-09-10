fd4c982bab2bd6784b59135b9bf7b80b88b7d04c
natsheol <145530295+natsheol@users.noreply.github.com>
2024-09-10 09:45:55 +0700

    Add files via upload

---
+++ pertemuan18_2/src/main/java/com/mycompany/pertemuan18_2/Pertemuan18_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan18_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan18_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
