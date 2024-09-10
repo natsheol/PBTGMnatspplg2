33ae967681bbcd9ead152667e6215666f4056a15
natsheol <145530295+natsheol@users.noreply.github.com>
2024-09-10 09:45:03 +0700

    Add files via upload

---
+++ pertemuan18_1trycatch/src/main/java/com/mycompany/pertemuan18_1trycatch/Pertemuan18_1trycatch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan18_1trycatch;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan18_1trycatch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
