db37738cc9680a0c920cac073ed20968566918b9
natsheol <145530295+natsheol@users.noreply.github.com>
2024-09-10 08:36:04 +0700

    Add files via upload

---
+++ pertemuan14_2/src/pertemuan14_2/Pertemuan14_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan14_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class Pertemuan14_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ pertemuan14_2/src/pertemuan14_2/burung.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan14_2;
+
+/**
+ *
+ * @author nasozu
+ */
+public class burung extends abstrakhewan {
+    @Override
+    public void suara() {
+        System.out.println("Suara Burung mencicit");
+ 
+    }
+}
